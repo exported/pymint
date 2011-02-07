35faf3c7dd644b2646c2340c152472a00db23c01
nativ.assaf@gmail.com <nativ.assaf@gmail.com@a5f4bc17-79e0-e6b3-0dbb-a028208ca486>
2011-02-07 17:09:54 +0000

    License stuff

--- trunk/win32structs.py
+++ trunk/win32structs.py
@@ -1,3 +1,25 @@
+#
+#   win32structs.py
+#
+#   pyMint - Remote process memory inspection python module
+#   https://code.google.com/p/pymint/
+#   Nativ.Assaf+pyMint@gmail.com
+#   Copyright (C) 2011  Assaf Nativ
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+
 # ImPoRtS
 
 # Import win32con
